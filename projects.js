81385ed417e1b0eaa2a93e1f78b1d156ff62bd5b
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2021-10-03 17:30:45 +0200

    my 2nd Portfolio

---
+++ projects.js
@@ -0,0 +1,14 @@
+document.getElementById("foot01").innerHTML = "<p>&copy; " + new Date().getFullYear() + " Eugene Mark Korku Barasu. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML = `<ul id="menu">
+    <li><a style="color:white;background-color:#009688;" href="index.html"><i class="fa fa-home"></i>Home</a></li>
+    <li><a href="project_9.html">Super-Calculator Project</a></li>
+    <li><a href="project_10.html">Word Replacement Game</a></li>
+    <li><a href="project_13.html">Wish List</a></li>
+    <li><a href="project_14.html">JavaScript Pizzaria</a></li>
+    <li><a href="project_15.html">Activity of the day Calender</a></li>
+    <li><a href="project_16.html">Adventure Game</a></li>
+    <li><a href="project_18.html">Lunch Game</a></li>
+    <li><a href="project_19.html">Lemonade Game</a></li>
+    <li><a href="project_shopStart.html">Shop-Start</a></li>    
+</ul>`;
